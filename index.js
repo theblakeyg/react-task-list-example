e8c0873924ee04f3495d79944644308222db2b98
theblakeyg <theblakeyg@gmail.com>
2024-03-15 13:37:01 -0500

    Get react up and running. Change file structure to work with react-scripts.

--- index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-import App from "./Container/AppFunction";
-
-ReactDOM.createRoot(
-  document.querySelector("#app")
-).render(<App />)
